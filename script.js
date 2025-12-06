be098c2089efada09862da3c8904fa2f49206c76
pankaj-python <pankajupy267@gmail.com>
2025-12-06 10:33:20 +0530

    Modified Javascript File

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
     function updateClock() {
         let now = new Date();
 
-        let hours = string(now.getHours()).toString().padStart(2, '0');
-        let minutes = string( now.getMinutes()).toString().padStart(2, '0');
-        let seconds = string(now.getSeconds()).toString().padStart(2, '0');
+        let hours = String(now.getHours()).toString().padStart(2, '0');
+        let minutes = String(now.getMinutes()).toString().padStart(2, '0');
+        let seconds = String(now.getSeconds()).toString().padStart(2, '0');
         document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}`;
     }
 
